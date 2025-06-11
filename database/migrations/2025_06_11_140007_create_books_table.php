d8b0235e107ae0b11d1fb62e450f4203a1027224
reuben <rbnvvcnt@gmail.com>
2025-06-11 22:40:12 +0700

    API book initial

---
+++ database/migrations/2025_06_11_140007_create_books_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('title')->unique();
+            $table->string('author');
+            $table->string('publisher')->nullable();
+            $table->year('publication_year')->nullable();
+            $table->text('cover')->nullable();
+            $table->text('description')->nullable();
+            $table->integer('price')->nullable();
+            $table->foreignId('created_by')->nullable()->constrained('users');
+            $table->foreignId('updated_by')->nullable()->constrained('users');
+            $table->foreignId('deleted_by')->nullable()->constrained('users');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
